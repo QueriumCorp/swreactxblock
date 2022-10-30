71c0db537f05449998742cf808ce2b2da8dc1c30
Kent Fuka <kent@kncl.com>
2022-10-30 15:46:08 -0600

    syntax error

--- swpwrxblock/swpwrxblock.py
+++ swpwrxblock/swpwrxblock.py
@@ -859,7 +859,7 @@ class SWPWRXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
         # window.swpwr_problem.steps[SWPHASE].hint3 = question.q_hint3;
         swpwr_problem['steps'][SWPHASE]['hint3'] = self.q_hint3
         # Emit the Python dict into the HTML as Javascript object
-        json_string = json.dumps(swpwr_problem,separators=(',', ':')))
+        json_string = json.dumps(swpwr_problem,separators=(',', ':'))
         js_string = '      window.swpwr_problem = `'+js_string+'`;`
         if DEBUG: logger.info("SWPWRXBlock student_view() swpwr_problem final json={j}".format(j=js_string))
         frag.add_javascript(js_string)     # SWPWR problem to work.
