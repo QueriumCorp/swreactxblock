76c11b38a1605117bdd54a914a8def2c54eeb0f0
Kent Fuka <kent@kncl.com>
2024-07-09 14:13:19 -0600

    change mimetype for module resource

--- swpwrxblock/swpwrxblock.py
+++ swpwrxblock/swpwrxblock.py
@@ -1064,7 +1064,7 @@ class SWPWRXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
             # frag.add_javascript_url("//s3.amazonaws.com/stepwise-editorial.querium.com/swpwr/public/index-YyiH-LRh.js")  # This gets a CORB error
             # frag.add_javascript_url("public/index-YyiH-LRh.js") # Need to update any time swpwr gets rebuilt
             # frag.add_javascript(self.resource_string("public/index-YyiH-LRh.js")) # Need to update any time swpwr gets rebuilt
-            frag.add_resource('<script type="module" src="' + self.resource_string("public/index-YyiH-LRh.js") + '"></script>','module','head')
+            frag.add_resource('<script type="module" src="' + self.resource_string("public/index-YyiH-LRh.js") + '"></script>','application/javascript','head')
             # frag.add_resource(final_string,'application/javascript','foot')
 
 #NOTYET         stepwise_setup_string = '''
