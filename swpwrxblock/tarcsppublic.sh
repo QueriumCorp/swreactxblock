baeb0c4dffa85099e874babb186377a35fe53dbd
Kent Fuka <kent@kncl.com>
2024-07-16 11:15:07 -0600

    change tardist.sh to tarcsppublic.sh

---
+++ swpwrxblock/tarcsppublic.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+#
+#Usage: tarscppublic.sh 
+#       Tars up the public directory so it can be synched to Amazon S3
+#       Uses scp to copy the archive to stepwise-editorial.querium.com
+#	After copying the dist files to stepwise-editorial, you can extract the contents into /var/www/swpwr
+d=`date +"%Y%m%d%H%M"`
+tar cvfz public-$d.tar.gz public
+scp public-$d.tar.gz root@stepwise-editorial.querium.com:/var/www/swpwr/
