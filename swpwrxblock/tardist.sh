0321f1d30952e049e51fed8dcdf112864b389fa5
Kent Fuka <kent@kncl.com>
2024-07-16 11:15:30 -0600

    remove tardist.sh

--- swpwrxblock/tardist.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-#
-#Usage: tardist.sh 
-#       Tars up the public directories so they can be synched to Amazon S3
-#       Uses scp to copy these to stepwise-editorial.querium.com
-#	After copying the dist files to stepwise-editorial, you can extract the contents into /var/www/swpwr
-d=`date +"%Y%m%d%H%M"`
-tar cvfz public-$d.tar.gz public
-scp public-$d.tar.gz root@stepwise-editorial.querium.com:/var/www/swpwr/
