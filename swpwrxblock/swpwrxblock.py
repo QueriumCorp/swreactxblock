a70e85da0a072f69103be303d7bb71280d5bf48c
Kent Fuka <kent@kncl.com>
2024-07-21 15:02:20 -0600

    fixup single quotes in swpwr problem definition to avoid bad structure

--- swpwrxblock/swpwrxblock.py
+++ swpwrxblock/swpwrxblock.py
@@ -999,7 +999,7 @@ class SWPWRXBlock(StudioEditableXBlockMixin, ScorableXBlockMixin, XBlock):
                                           'title: "' + 'SAMPLE' + '", ' + \
                                           'stimulus: "' + self.q_stimulus + '", ' + \
                                           'topic: "' + 'gradeBasicAlgebra' + '", ' + \
-                                          'definition: \'' + self.q_definition + '\', ' + \
+                                          'definition: \'' + str(self.q_definition).replace('\'', '&apos;') + '\', ' + \
                                           'mathHints: "' + '[]' + '",' + \
                                           'wpHints: "' + self.q_swpwr_problem_hints + '"' + \
                                         ' },' + \
