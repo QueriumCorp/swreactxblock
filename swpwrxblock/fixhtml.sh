dc0fdeff93e4ab1ae1678955640fd50e16914d9a
Kent Fuka <kent@kncl.com>
2024-08-13 14:00:12 -0600

    Update swpwr xblock to 1.9.84

---
+++ swpwrxblock/fixhtml.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ $# -ne 2 ];
+    then echo "wrong number of parameters. must specify the javascript filename and the CSS filename to use."
+fi
+#    <script type=module crossorigin src=/static/xblock/resources/swpwrxblock/public/index-CIjUOCFG.js></script>
+#    <link rel=stylesheet crossorigin href=/static/xblock/resources/swpwrxblock/public/index-KWNQs58g.css>
+sed -i .bak -e 's#index-.*\\.js#$1#g' static/html/swpwrxstudent.html
+sed -i .bak2 -e 's#index-.*\\.css#$2#g' static/html/swpwrxstudent.html
