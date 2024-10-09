e1d39cc4705393b3275015bf25d8f25759e1886a
Kent Fuka <kent@kncl.com>
2024-10-09 16:11:27 -0600

    deal with both in the git status output

--- swpwrxblock/updateme2.sh
+++ swpwrxblock/updateme2.sh
@@ -1,6 +1,6 @@
 /tarscppublic.sh
-git status | grep 'deleted:' | grep public | sed -e 's/deleted:/git rm /' | bash
-git status | grep 'modified:' | grep public | sed -e 's/modified:/git add /' | bash
+git status | grep 'deleted:' | grep public | sed -e 's/both //' -e 's/deleted:/git rm /' | bash
+git status | grep 'modified:' | grep public | sed -e 's/both //' -e 's/modified:/git add /' | bash
 git add static/html/swpwrxstudent.html
 git add public
 cat public/swpwr_version.json
