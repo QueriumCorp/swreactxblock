bbb4090c5fc1d9d897a71d280753c42283d51653
lpm0073 <lpm0073@gmail.com>
2024-11-12 18:15:58 -0600

    fix: broaden exceptions

--- swpwrxblock/__init__.py
+++ swpwrxblock/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """Note that importing SWPWRXBlock is a requirements of XBlock SDK."""
 
+# pylint: disable=W0718,C0103
 try:
     from .swpwrxblock import SWPWRXBlock  # noqa: F401
-except (ImportError, ModuleNotFoundError):
-    print("swpwrxblock.__init__.py - ImportError: SWPWRXBlock")
+except Exception as e:
+    description = str(e)
+    print(f"swpwrxblock.__init__.py - ImportError: SWPWRXBlock {description}")
