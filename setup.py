b82c08ac31eef448a63b98ed0e1e147ec6fbedb1
lpm0073 <lpm0073@gmail.com>
2024-11-14 12:12:36 -0600

    chore: bump to 18.1.22

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 
 setup(
     name=PACKAGE_NAME,
-    version="18.1.21",
+    version="18.1.22",
     description="Stepwise Power XBlock",
     license="MIT",
     install_requires=["XBlock", "requests"],
